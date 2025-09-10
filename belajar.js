30c90c736bfdbf755de400e872b5f8267ccaddb9
Arya Putra Wijaya <legendoffreefire60@gmail.com>
2025-09-10 02:17:53 -0600

    File hari ini

---
+++ belajar.js
@@ -0,0 +1,9 @@
+alert ("Halo semuanya!");
+confirm("apakah kamu mau belajar Java Script");
+prompt("Masukkan identitas kamu","Air Kesucian langit");
+function foo() {
+    alert("Ini merupakan website Java Sript!");
+}foo();a
+Date("15 Juli 2005");
+parseInt("5");
+setTimeout(function(){alert("ini merupakan alert dari setTimeout");},3000);
