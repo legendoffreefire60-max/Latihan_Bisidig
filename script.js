11eae28932af1e93889ddfeecf215790b8116cca
Arya Putra Wijaya <legendoffreefire60@gmail.com>
2025-09-11 20:46:09 -0600

    Ini file JS

---
+++ script.js
@@ -0,0 +1,120 @@
+
+
+
+
+
+// INI MERUPAKAN TUGAS JAVA SCRIPT
+
+// DAN INI VARIABELNYA
+
+var no1 = 5;
+no1 = 3;
+console,log(no1); //3
+//window.alert(no1); //3
+
+console,log("=========ini penjumlahan=========");
+no1 = no1 + 11;
+console.log(no1);//ini hasilnya adalah 14 karena 3 + 11 = 14
+
+console.log("=========ini pengurangan==========");
+no1 = no1 - 7;
+console.log(no1);// ini hasilnya adalah 7 karena 14 - 7 = 7
+
+console.log("===========ini adalah perkalian==========");
+var no2 = no1 * 10;
+console.log(no2); // INI ADALAH HASIL 70 KARENA 7 * 10 = 70
+
+console.log("===========ini merupakan pembagian===========");
+var no3 = no2/no1;
+console.log(no3);//ini hasilnya 10 karena 70/7= 10
+
+//ini type Variabel
+console.log("=======ini variabel string=========");
+varString= "Hallo I am Arya Putra Wijaya";
+console.log(varString);
+
+console.log("==========ini merupakan variabel integer=========");
+var myinterger=10.5;//ini tipe data integer
+console.log(myinterger);
+
+console.log("=========ini variabel float=========");
+var myinterger=10.5;//ini merupakan tipe data float
+console.log(myFloat);
+
+console.log("==========ini variabel Long atau panjang==========");
+var myLong=12345678901234567890n;//ini merupakan tipe data long
+console.long(myLong);
+
+console.log("==========ini merupakan variabel boolean");
+var myBoolean=true;//ini merupakan tipe data boolean
+console.log(myBoolean);
+
+console.log("==========ini merupakan variabel boolean 2");
+var myBoolean2=false;//ini merupakan tipe data boolean
+console.log(myBoolean2);
+
+console.log("=========not==========");
+var myUndefined;//ini merupakan tipe data undefined
+console.log(myUndefined);
+
+console.log("===========ini merupakan NUll==========");
+var myNull=null;//ini merupakan data null
+console.log(myNull);
+
+console.log("===========ini merupakan Double=========");
+var myDouble=931014141982.22;//ini merupakan tipe data double
+console.log(myDouble);
+
+console.log("===========ini merupakan NaN");
+var NaN_Example = 0/0;//ini merupakan tipe data NaN(Not a Number)
+console.log(NaN_Example);
+
+console.log("==============");
+console.log("Hello World");
+console.log("==============");
+
+//ini Array dan Object
+console.log("================");
+var myArray=[];//empety array
+console.log(myArray);
+
+console.log("================")
+var favoriteName = ["Arya","Putra","Wijaya"];
+console.log(favoriteName);
+
+console.log("===========")
+var carsinParkingLot=["Superman","Batman","Hulk"];
+console.log(carsinParkingLot);
+
+console.log("=============");
+var employees=["Arya","Abim","Basori"];
+console.log(employees);
+
+console.log("=================");
+var primeNumbers=[2,3,5,7,11,13,17,19,23,29,31];
+console.log(primeNumbers);
+
+console.log("================");
+var randomVariabels=[2,"any typeworks",undefined,null,true,2.51];
+myArray=["Zero","One","Two"];
+console.log(randomVariables);
+
+window.alert(myArray[0]);//0
+ElementNumber=1;
+window.alert(myArray[ElementNumber]);//Arya
+myObject={};
+
+john={firstname:"John",lastname:"Doe",fullname:"JohnDoe"};
+billy={firstname:"Billy",lastname:undefined,fullname:"Billy"};
+window.alert(john.fullname);//john Doe
+window.alert(billy.firstname);//Billy
+
+alert("Halo semuanya");
+confirm("Apakah masih ingin melanjutkan?");
+prompt("Masukkan nama anda;","");
+function foo() {
+alert("Pengoperasian sudah selesai");
+}foo();a
+Date("2005,15,07");
+parseInt("2");
+setTimeout(function(){ alert("Ini merupakan alat tools dari setTimeout");},3000);
